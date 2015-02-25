26fb7b9ef0ff8c0b45ef8f81892e16c3dbcfe4bd
jasminenoack <jasminenoack@gmail.com>
2015-02-24 23:23:20 -0500

    selects the block clicked on

--- jquery/lib/play.js
+++ jquery/lib/play.js
@@ -1,11 +1,12 @@
 ;(function (){
 
 if (window.Bejeweled === undefined) {
-  window.Bejeweled = {}
+  window.Bejeweled = {};
 }
 
 var setup = Bejeweled.setup = function () {
   this.game = new Bejeweled.Game;
+  this.selected = undefined;
 }
 
 var draw = setup.prototype.draw = function () {
@@ -17,17 +18,18 @@ var draw = setup.prototype.draw = function () {
     for (var j = 0; j < 8; j++) {
       var block = blocks[i][j]
       var $block = $("<block></block>");
+      var pos = (" " + block.pos[0] + block.pos[1]).substring(1)
 
-      $block.css("background", block.color)
-      $block.css("display", "block")
-      $block.css("float", "left")
-      $block.css("height", "60px")
-      $block.css("width", "60px")
-      $block.css("border-radius", "5px")
-      $block.css("border", "1px solid black")
-      $block.addClass(" " + block.pos[0] + block.pos[1])
-      $block.css("margin", "1px")
-      $block.click(setup.handleClick.bind(this))
+      $block.css("background", block.color);
+      $block.css("display", "block");
+      $block.css("float", "left");
+      $block.css("height", "60px");
+      $block.css("width", "60px");
+      $block.css("border-radius", "5px");
+      $block.css("border", "1px solid black");
+      $block.attr("id", pos);
+      $block.css("margin", "1px");
+      $block.click(setup.handleClick.bind(this));
 
       board.append($block);
     }
@@ -36,9 +38,30 @@ var draw = setup.prototype.draw = function () {
 
 setup.handleClick = function (event) {
   var game = this.game;
-  var block = $(event.target);
+  var $block = $(event.target);
+  var id = $block.attr('id')
+  var block = game.blocks[id[0]][id[1]]
 
-  console.log (block)
+  if (this.selected === undefined) {
+    this.select(block);
+  } else if (
+    this.selected.pos[0] === block.pos[0] &&
+    this.selected.pos[1] === block.pos[1]
+  ) {
+    this.unselect(block);
+  } else {
+    this.select(block);
+  }
+
+  console.log(this.selected)
+}
+
+setup.prototype.select = function (block) {
+  this.selected = block;
+}
+
+setup.prototype.unselect = function (block) {
+  this.selected = undefined;
 }
 
 
