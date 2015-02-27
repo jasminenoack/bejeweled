d4ce43086deb4a66af77b9f082c5bb8cebb2c410
App Academy Student <appacademy@Apps-Mac-mini-37.local>
2015-02-27 09:35:21 -0500

    reformatting code

--- jquery/lib/blocks.js
+++ jquery/lib/blocks.js
@@ -1,38 +1,38 @@
-;(function () {
-
-if (window.Bejeweled === undefined) {
-  window.Bejeweled = {}
-};
-
-
-var block = Bejeweled.Block = function (color, pos, game) {
-  this.pos = pos;
-  this.color = color;
-  this.game = game;
-}
-
-block.COLORS = ["red", "green", "blue", "yellow", "orange", "purple"]
-
-block.create = function (pos, game) {
-  var color = block.COLORS[Math.floor(Math.random() * block.COLORS.length)]
-  return new block(color, pos, game)
-}
-
-block.prototype.id = function (){
-  // console.log(this)
-  return (" " + this.pos[0] + this.pos[1]).substring(1)
-}
-
-block.prototype.switch = function (otherBlock) {
-  start = this.pos;
-  end = otherBlock.pos;
-
-  var temp = this;
-  this.game.blocks[start[0]][start[1]] = otherBlock;
-  otherBlock.pos = start
-
-  this.game.blocks[end[0]][end[1]] = temp;
-  this.pos = end
-}
-
-})();
+// ;(function () {
+//
+// if (window.Bejeweled === undefined) {
+//   window.Bejeweled = {}
+// };
+//
+//
+// var block = Bejeweled.Block = function (color, pos, game) {
+//   this.pos = pos;
+//   this.color = color;
+//   this.game = game;
+// }
+//
+// block.COLORS = ["red", "green", "blue", "yellow", "orange", "purple"]
+//
+// block.create = function (pos, game) {
+//   var color = block.COLORS[Math.floor(Math.random() * block.COLORS.length)]
+//   return new block(color, pos, game)
+// }
+//
+// block.prototype.id = function (){
+//   // console.log(this)
+//   return (" " + this.pos[0] + this.pos[1]).substring(1)
+// }
+//
+// block.prototype.switch = function (otherBlock) {
+//   start = this.pos;
+//   end = otherBlock.pos;
+//
+//   var temp = this;
+//   this.game.blocks[start[0]][start[1]] = otherBlock;
+//   otherBlock.pos = start
+//
+//   this.game.blocks[end[0]][end[1]] = temp;
+//   this.pos = end
+// }
+//
+// })();
